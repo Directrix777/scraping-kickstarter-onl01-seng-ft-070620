9fdb5397488c25434b5efd0d0054e719eb1882ae
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-27 18:57:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,14 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each{|project|
+    projects[project] = {}
+  }
+
+  projects
 end
 
 create_project_hash
